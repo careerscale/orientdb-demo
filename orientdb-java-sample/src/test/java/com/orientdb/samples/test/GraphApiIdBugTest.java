d7115932aa6b43e677ed119bf714f1676124a461
Harinath Mallepally <hmallepa@calliduscloud.com>
2017-05-15 22:38:20 -0700

    unit test for sequence generation

---
+++ orientdb-java-sample/src/test/java/com/orientdb/samples/test/GraphApiIdBugTest.java
@@ -0,0 +1,42 @@
+package com.orientdb.samples.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
+
+@Test
+public class GraphApiIdBugTest {
+
+
+    public static final String CLASS_PREFIX = "class:";
+    public static final String USER = "User";
+    public static final String NAME = "name";
+
+
+    @Test
+    public void testIdBug() {
+        OrientGraph graph = new OrientGraphFactory("remote:localhost/demo", "root", "cloud").setupPool(1, 10).getTx();
+        // graph.addVertex(id, prop)
+
+
+        graph.addVertex(CLASS_PREFIX + USER, createProperties());
+        graph.addVertex(CLASS_PREFIX + USER, createProperties());
+        graph.addVertex(CLASS_PREFIX + USER, createProperties());
+        graph.addVertex(CLASS_PREFIX + USER, createProperties());
+
+        graph.commit();
+
+    }
+
+    private Map<String, Object> createProperties() {
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put(NAME, "first +  last");
+
+        return props;
+    }
+
+}
