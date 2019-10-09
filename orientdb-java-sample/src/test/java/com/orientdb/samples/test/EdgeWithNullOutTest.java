c8645c7c968a37adfb05e4c0c81aa53ce73b9259
Harinath Mallepally <harinath.mallepally@sap.com>
2019-10-09 14:13:11 -0700

    updated with DB creation test scenario

--- orientdb-java-sample/src/test/java/com/orientdb/samples/test/EdgeWithNullOutTest.java
+++ orientdb-java-sample/src/test/java/com/orientdb/samples/test/EdgeWithNullOutTest.java
@@ -10,8 +10,14 @@ import org.apache.tinkerpop.gremlin.orientdb.executor.OGremlinResultSet;
 import org.apache.tinkerpop.gremlin.structure.Direction;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.orientechnologies.orient.core.db.ODatabaseType;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 
 
 public class EdgeWithNullOutTest {
@@ -21,16 +27,44 @@ public class EdgeWithNullOutTest {
     private static OrientGraphFactory factory = null;
 
     @BeforeClass
-
-    public static OrientGraphFactory setup() {
+    public static void setup() {
 
         factory = new OrientGraphFactory("memory:mlm", "admin", "admin").setupPool(1, 10);
 
         OrientGraph graph = factory.getNoTx();
 
+        // String creds = "memory:/Users/i500221/git/orientdb-demo/orientdb-java-sample/yourdatabase";
+        String creds = "memory:yourdatabase";
+        ODatabaseDocumentTx db = new ODatabaseDocumentTx(creds);
+        if (!db.exists()) {
+            db.create();
+
+        }
+        try {
+            db.drop();
+        } catch (Exception e) {
+            System.out.println("test " + e.getMessage());
+        }
+
+        // OrientDB odb = new OrientDB(creds, "admin", "admin");
+
+        // OrientDB orientDb = new OrientDB("remote:localhost","root","root",
+        // OrientDBConfig.defaultConfig());
+
+        OrientDB orientDb = new OrientDB("plocal: /apps/orientdb/databases", "root", "root", OrientDBConfig.defaultConfig());
+        orientDb.createIfNotExists("test", ODatabaseType.PLOCAL);
+
+        ODatabaseDocument session = orientDb.open("test", "admin", "admin");
+        // ...
+        session.close();
+        orientDb.drop("test");
+        orientDb.close();
+
+
+
         setupDbSchema(graph);
 
-        return factory;
+        // return factory;
 
     }
 
@@ -72,8 +106,8 @@ public class EdgeWithNullOutTest {
 
             while (edges.hasNext()) {
                 Edge e = edges.next();
-                System.out.println(v + " id is " + v.property("id").toString() + " edge id is "
-                        + e.property("id").toString() + e.outVertex().property("id").toString());
+                System.out.println(v + " id is " + v.property("id").toString() + " edge id is " + e.property("id").toString()
+                        + e.outVertex().property("id").toString());
 
             }
             // assertEquals(edge.value("status").toString(), "1");
@@ -92,12 +126,10 @@ public class EdgeWithNullOutTest {
 
         noTxGraph.executeSql("ALTER DATABASE DATETIMEFORMAT \"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\"");
         noTxGraph.executeSql("CREATE CLASS BV EXTENDS V");
-        noTxGraph.executeSql(
-                "CREATE PROPERTY BV.createdDate DATETIME (MANDATORY TRUE, default sysdate(\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\"));");
+        noTxGraph.executeSql("CREATE PROPERTY BV.createdDate DATETIME (MANDATORY TRUE, default sysdate(\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\"));");
         noTxGraph.executeSql("CREATE PROPERTY BV.updatedDate DATETIME (MANDATORY FALSE);");
         noTxGraph.executeSql("CREATE CLASS BE EXTENDS E;");
-        noTxGraph.executeSql(
-                "CREATE PROPERTY BE.createdDate DATETIME (MANDATORY TRUE, default sysdate(\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\"));");
+        noTxGraph.executeSql("CREATE PROPERTY BE.createdDate DATETIME (MANDATORY TRUE, default sysdate(\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\"));");
         noTxGraph.executeSql("CREATE PROPERTY BE.updatedDate DATETIME (MANDATORY FALSE);");
 
 
@@ -105,23 +137,20 @@ public class EdgeWithNullOutTest {
         noTxGraph.executeSql("CREATE SEQUENCE partOfIdSequence TYPE ORDERED;");
         noTxGraph.executeSql("CREATE CLASS PART_OF EXTENDS BE;");
         noTxGraph.executeSql("CREATE PROPERTY PART_OF.name STRING (MANDATORY TRUE, MIN 3, MAX 50);");
-        noTxGraph.executeSql(
-                "CREATE PROPERTY PART_OF.id LONG (MANDATORY TRUE, default \"sequence('partOfIdSequence').next()\");");
+        noTxGraph.executeSql("CREATE PROPERTY PART_OF.id LONG (MANDATORY TRUE, default \"sequence('partOfIdSequence').next()\");");
 
 
         noTxGraph.executeSql("CREATE CLASS Market EXTENDS BV;");
         noTxGraph.executeSql("CREATE SEQUENCE marketIdSequence TYPE ORDERED;");
         noTxGraph.executeSql("CREATE PROPERTY Market.name STRING (MANDATORY TRUE, MIN 3, MAX 50);");
-        noTxGraph.executeSql(
-                "CREATE PROPERTY Market.id LONG (MANDATORY TRUE, default \"sequence('marketIdSequence').next()\");");
+        noTxGraph.executeSql("CREATE PROPERTY Market.id LONG (MANDATORY TRUE, default \"sequence('marketIdSequence').next()\");");
         noTxGraph.executeSql("CREATE PROPERTY Market.code STRING;");
 
         noTxGraph.executeSql("CREATE INDEX MARKET_ID_INDEX ON Market (id BY VALUE) UNIQUE;");
         noTxGraph.executeSql("CREATE CLASS Country EXTENDS BV;");
         noTxGraph.executeSql("CREATE SEQUENCE countryIdSequence TYPE ORDERED;");
         noTxGraph.executeSql("CREATE PROPERTY Country.name STRING (MANDATORY TRUE, MIN 3, MAX 50);");
-        noTxGraph.executeSql(
-                "CREATE PROPERTY Country.id LONG (MANDATORY TRUE, default \"sequence('countryIdSequence').next()\");");
+        noTxGraph.executeSql("CREATE PROPERTY Country.id LONG (MANDATORY TRUE, default \"sequence('countryIdSequence').next()\");");
         noTxGraph.executeSql("CREATE PROPERTY Country.code STRING (MANDATORY TRUE, MIN 2, MAX 3);");
         noTxGraph.executeSql("CREATE INDEX Country_ID_INDEX ON Country (id BY VALUE) UNIQUE;");
 
